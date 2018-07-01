21061fa046666d17f0eabd86540f5a84f9abfc06
leniumd <websecuritix@gmail.com>
2018-06-30 23:40:16 -0700

    update genesis block setup

--- contrib/setup-genesis.sh
+++ contrib/setup-genesis.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-TIMESTAMP=1530151500
-NEWS=" CoinTelegraph 28/June/2018 Apple’s Steve Wozniak Calls Blockchain a ‘Bubble,’ Thinks Bitcoin Is Still ‘Just Amazing’."
-PUBLICKEY="040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9"
+TIMESTAMP=1530347040
+NEWS="Bloomberg 30/June/2018 Bitcoin Jumps Back Above 6,000 USD to Give Respite to Investors"
 AMOUNT=5000000000
-python gen.py -a X11 -t $TIMESTAMP -z $NEWS -p $PUBLICKEY -v $AMOUNT
-
-# generate keys
+python gen.py -a X11 -t $TIMESTAMP -z $NEWS -v $AMOUNT
